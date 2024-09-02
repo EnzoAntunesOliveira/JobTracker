abd2477d4e5c4a207510303f786bebca0e568185
yagoluucas <yago543@gmail.com>
2024-09-02 09:47:51 -0300

    alterado arquivo layout

--- src/main/java/com/example/app_vagas/MainActivity.kt
+++ src/main/java/com/example/app_vagas/MainActivity.kt
@@ -10,9 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
 class MainActivity : AppCompatActivity() {
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,29 +25,30 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.inflateMenu(R.menu.bottom_nav_menu)
 
-        // Configuração do listener para o clique nos itens do menu
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            when(item.itemId){
-                R.id.navigation_filter -> {
-                    setContentView(R.layout.activity_filter)
-                    val btnClosePage: ImageView = findViewById<ImageView>(R.id.ID_Filter_ClosePage)
 
-                    btnClosePage.setOnClickListener(){
-                        setContentView(R.layout.activity_main)
-                    }
-                    true
-                }
-                R.id.navigation_alert -> {
-                    setContentView(R.layout.activity_notification)
-                    true
-                }
-                R.id.navigation_profile -> {
-                    setContentView(R.layout.activity_profile)
-                    true
-                }
-                else -> false
-            }
-        }
+        // Configuração do listener para o clique nos itens do menu
+//        bottomNavigationView.setOnItemSelectedListener { item ->
+//            when(item.itemId){
+//                R.id.navigation_filter -> {
+//                    setContentView(R.layout.activity_filter)
+//                    val btnClosePage: ImageView = findViewById<ImageView>(R.id.ID_Filter_ClosePage)
+//
+//                    btnClosePage.setOnClickListener(){
+//                        setContentView(R.layout.activity_main)
+//                    }
+//                    true
+//                }
+//                R.id.navigation_alert -> {
+//                    setContentView(R.layout.activity_notification)
+//                    true
+//                }
+//                R.id.navigation_profile -> {
+//                    setContentView(R.layout.activity_profile)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
 
 
     }
